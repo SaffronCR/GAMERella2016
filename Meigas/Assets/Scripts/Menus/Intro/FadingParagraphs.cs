73aa8f4fda85f3cbc41ca01bc7f7a184b575d975
Alicia G <alicia.guardenoalbertos@gmail.com>
2016-11-12 18:47:11 +0100

    Added basic intro scene with text fading (for story purposes)

---
+++ Meigas/Assets/Scripts/Menus/Intro/FadingParagraphs.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FadingParagraphs : MonoBehaviour {
+
+    private Queue<Text> paragraphs;
+    private float movementParagraph = 50;
+    [SerializeField]
+    private float timeTakenDuringLerp = 2f;
+    [SerializeField]
+    private float timeShowingTextUntilGameStarts = 1f;
+
+    private Text _test;
+
+    private void Awake()
+    {
+        paragraphs = new Queue<Text>();
+        for(int i = 0; i < this.transform.childCount; i++)
+        {
+            Text paragraph = this.transform.GetChild(i).GetComponent<Text>();
+            Color _color = paragraph.color;
+            _color.a = 0;
+            paragraph.color = _color;
+
+            paragraphs.Enqueue(paragraph);
+        }
+    }
+
+	private void Start () {
+        StartCoroutine(FadingCoroutine());
+	}
+
+    private IEnumerator FadingCoroutine()
+    {
+        bool isLerping;
+
+        while (paragraphs.Count > 0)
+        {
+            Text currentParagraph = paragraphs.Dequeue();
+            Vector2 currentParagraphPosition = currentParagraph.rectTransform.anchoredPosition;
+            float endPositionX = currentParagraph.rectTransform.anchoredPosition.x + movementParagraph;
+            Color _color = currentParagraph.color;
+
+            float _timeStartedLerping = Time.time;
+            isLerping = true;
+
+            while (isLerping)
+            {
+                float timeSinceStarted = Time.time - _timeStartedLerping;
+                float percentageComplete = timeSinceStarted / timeTakenDuringLerp;
+
+                currentParagraphPosition.x = Mathf.Lerp(currentParagraph.rectTransform.anchoredPosition.x, endPositionX, percentageComplete);
+                _color.a = Mathf.Lerp(currentParagraph.color.a, 1, percentageComplete);
+
+                currentParagraph.rectTransform.anchoredPosition = currentParagraphPosition;
+                currentParagraph.color = _color;
+
+                if(percentageComplete >= 1f)
+                {
+                    isLerping = false;
+                }
+
+                yield return null;
+            }
+        }
+
+        yield return new WaitForSeconds(timeShowingTextUntilGameStarts);
+        //SceneManager.LoadScene("Game");
+    }
+
+}
