52b3eca3498b9e2f600a2ef768f2d75fa7983b68
Alicia G <alicia.guardenoalbertos@gmail.com>
2016-11-13 20:46:58 +0100

    Added credits to main menu scene, created first final main menu scene

---
+++ Meigas/Assets/Scripts/Menus/MainMenu/CreditsButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : MainMenuPanelButton
+{
+
+    protected void OnEnable()
+    {
+        MenusInputController.deselectMainPanelButtons += Deselect;
+        MenusInputController.selectCreditsButton += Select;
+    }
+
+    protected void OnDisable()
+    {
+        MenusInputController.deselectMainPanelButtons -= Deselect;
+        MenusInputController.selectCreditsButton -= Select;
+    }
+}
