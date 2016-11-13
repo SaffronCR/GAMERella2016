4c4b6b723b4fd4cd2492fba9cd10923af3725f07
Alicia G <alicia.guardenoalbertos@gmail.com>
2016-11-13 22:44:30 +0100

    Added read score script, final intro and game over scenes

---
+++ Meigas/Assets/Scripts/Menus/GameOver/ReadScore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ReadScore : MonoBehaviour {
+
+    private Text scoreText;
+
+	private void Awake() {
+        scoreText = this.GetComponent<Text>();
+	}
+
+    private void Start()
+    {
+        scoreText.text = "Saved Souls\n" + PlayerPrefs.GetFloat("score"); ;
+    }
+
+}
